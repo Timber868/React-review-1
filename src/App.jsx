be18eef77e9c35498dbc19b2c1ed739362489849
Timber868 <Timroma6@gmail.com>
2024-01-29 23:02:00 -0500

    Added core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Header from "./components/Header";
 import genRandomInt from "./components/GenRandomInt";
+import CoreConcepts from "./components/CoreConcepts";
+import { CORE_CONCEPTS } from "./components/data";
+
 
 function App() {
   const wordOptions = ["Way", "Time", "No better time"]
@@ -10,6 +13,15 @@ function App() {
       <Header/>
       <main>
         <h2>{firstWord} to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import reactImg from "../assets/react-core-concepts.png"
+
 function Header(){
     
     return(
         <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
