6915d05cf18f6a974eb9f8a1e999bb7b0d708650
Timber868 <Timroma6@gmail.com>
2024-01-29 22:21:36 -0500

    Added a random aspect to my website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Header from "./Header";
+import Header from "./components/Header";
+import genRandomInt from "./components/GenRandomInt";
 
 function App() {
+  const wordOptions = ["Way", "Time", "No better time"]
+  const firstWord = wordOptions[genRandomInt(3)];
+
   return (
     <div>
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+        <h2>{firstWord} to get started!</h2>
       </main>
     </div>
   );
